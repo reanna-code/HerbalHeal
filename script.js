e9479a2eb558a923b3609f3d3faeef502c8009d4
reanna-code <83554897+reanna-code@users.noreply.github.com>
2024-02-25 11:45:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // script.js
 
 function searchRemedies(event) {
-    event.preventDefault();
-    const searchTerm = document.getElementById('searchInput').value;
-    // You can perform additional validation or processing here if needed
+  event.preventDefault();
+  const searchTerm = document.getElementById('searchInput').value;
+  // You can perform additional validation or processing here if needed
 
-    // Redirect to the results page with the search term as a query parameter
-    window.location.href = `results.html?search=${searchTerm}`;
+  // Redirect to the results page with the search term as a query parameter
+  window.location.href = `results.html?search=${searchTerm}`;
 }
+
+document.querySelector('.nav-links li:last-child a').addEventListener('click', function(event) {
+  event.preventDefault();
+  const target = document.getElementById('bottom');
+  target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
